dc8683c4f84c84b98a9897f55c83651c314e5845
Lanny <rjenkins@mail.sfsu.edu>
2019-11-27 09:28:18 -0800

    Added ChemDetails stub

---
+++ space-viewer/src/chem-details/ChemDetails.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Placeholder = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`
+
+const ChemicalName = styled.h2`
+  border-bottom: 1px solid black;
+  margin-bottom: 5px;
+  padding: 10px 10px 5px 10px;
+`
+
+const ChemDetails = ({ chem }) => {
+  if (!chem) {
+    return (
+      <Placeholder>
+        <span>Select a Chemical to View Details</span>
+      </Placeholder>
+    )
+  }
+}
+
+export default ChemDetails
--- space-viewer/src/space-viewer/SpaceViewer.js
+++ space-viewer/src/space-viewer/SpaceViewer.js
@@ -2,17 +2,28 @@ import React from 'react'
 import styled from 'styled-components'
 
 import ChemList from '../chem-list/ChemList'
+import ChemDetails from '../chem-details/ChemDetails'
 import SpaceScene from '../space-scene/SpaceScene'
 
-const ViewerContainer = styled.div`
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const TopPane = styled.div`
   display: flex;
   width: 100%;
   height: 600px;
 `
 
+const BottomPane = styled.div`
+  height: 200px;
+`
+
 const SpaceViewer = ({ facetDataUrl }) => {
   const [facet, setFacet] = React.useState()
   const [chemicals, setChemicals] = React.useState()
+  const [selectedChem, setSelectedChem] = React.useState(-1)
 
   React.useEffect(() => {
     fetch(CSpace.facetDataUrl)
@@ -24,10 +35,19 @@ const SpaceViewer = ({ facetDataUrl }) => {
   }, [])
 
   return (
-    <ViewerContainer>
-      <SpaceScene facet={facet} chemicals={chemicals} />
-      <ChemList chemicals={chemicals} />
-    </ViewerContainer>
+    <Container>
+      <TopPane>
+        <SpaceScene facet={facet} chemicals={chemicals} />
+        <ChemList
+          chemicals={chemicals}
+          selectedChem={selectedChem}
+          setSelectedChem={setSelectedChem}
+        />
+      </TopPane>
+      <BottomPane>
+        <ChemDetails chem={chemicals && chemicals[selectedChem]} />
+      </BottomPane>
+    </Container>
   )
 }
 
